7368dd7b31f144ff8b4814029d212fde4e701537
Carl J <carlj1881@yahoo.com>
2020-09-29 05:50:41 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -38,10 +38,10 @@ class Song
   end
 
   def self.find_or_create_by_name(name)
-       self.create_by_name(name)
+    self.create_by_name(name)
     self.find_by_name(name)
-
   end
 
+  def self.alphabetical(file)
 
 end

be0304cae2309d3da459b6847e3f04dc2354010d
Carl J <carlj1881@yahoo.com>
2020-09-29 03:44:32 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,28 @@
+require"pry"
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
-
+  
+  
   def self.all
     @@all
   end
 
   def save
     self.class.all << self
+    self
+  end
+
+  def self.create
+    song = Song.new
+    song.save
+  end
+  
+  def self.new_by_name(name)
+    binding.pry
+    song = self.new
+    self
   end
 
 end

db23f9f4a34b7d33d099b7080052775ffac0c3c1
Carl J <carlj1881@yahoo.com>
2020-09-29 05:14:58 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -38,11 +38,6 @@ class Song
     self.all.find{|song| song.name == name}
   end
 
-  def self.find_or_create_by_name(name)
-    if self.find_by_name 
-    else self.create_by_name
-      self
-    end
-  end
+
 
 end
